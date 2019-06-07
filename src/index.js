cdd1321ec06c8a0528bf59ac8553a6313371a777
Ricardo Cruz <ricardo.mj.cruz@gmail.com>
2019-06-07 11:38:22 +0100

    friday

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-   
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-        >
-        {this.props.value}
-        </button>
-      );
-    }
+
+/*Continue in Taking Turns - https://reactjs.org/tutorial/tutorial.html */
+
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
+
   
   class Board extends React.Component {
     constructor(props){
